e456b9585e0e71b1e6a1ecfeb096382d30c0f42f
wileybaba <wileymckayconte@gmail.com>
2019-07-12 13:49:46 -0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import OrderBookTable from './components/OrderBookTable'
+
+function App() {
+  return (
+    <div className="App">
+      <OrderBookTable />
+    </div>
+  );
+}
+
+export default App;
